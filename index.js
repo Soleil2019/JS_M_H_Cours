74a3e848707280241c0877d281791464878d48f4
Malek CHILLA <malekchilla2019@gmail.com>
2019-09-23 17:56:42 +0100

    news objects

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 // 4-4-Dynamic Types:
 // 2 languages static where variable don't changes and dynamic where variable chages
+// typeof variables: string, numbers, boolean, undifined, null
+// null is an object
 
 let name = 'Mosh';
 let age = 30;

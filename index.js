114d6a4ffdd177f79f122b98307563e10e0c5652
Malek CHILLA <malekchilla2019@gmail.com>
2019-09-23 17:34:33 +0100

    dynamic types

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
+// 4-4-Dynamic Types:
+// 2 languages static where variable don't changes and dynamic where variable chages
+
 let name = 'Mosh';
+let age = 30;
+let isApproved = false;
+let firstName = undefined;
+let selectedColor = null;

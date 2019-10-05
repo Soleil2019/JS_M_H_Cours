8348bdea6ad55dd00e9960e0d93d0488063a1932
Malek CHILLA <malekchilla2019@gmail.com>
2019-10-05 14:03:22 +0100

    add objects

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
-// 4-4-Dynamic Types:
-// 2 languages static where variable don't changes and dynamic where variable chages
-// typeof variables: string, numbers, boolean, undifined, null
-// null is an object
+//objects
 
-let name = 'Mosh';
-let age = 30;
-let isApproved = false;
-let firstName = undefined;
-let selectedColor = null;
+let person = {
+  name : 'Mosh',
+  age : 30
+}

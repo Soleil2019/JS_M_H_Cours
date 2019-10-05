4a82fd7e9d3e4d0d2d180a4cdb278a44df43ac34
Malek CHILLA <malekchilla2019@gmail.com>
2019-10-05 14:59:08 +0100

    add arithmetic operators

---
+++ 03_operators/arithmetics_operatos/index.js
@@ -0,0 +1,18 @@
+let x = 10;
+let y = 3;
+
+// console.log(x + y);
+// console.log(x - y);
+// console.log(x * y);
+// console.log(x / y);
+// console.log(x % y);
+// console.log(x ** y);
+
+// increment (++)
+// console.log(x++);
+// console.log(x);
+
+// Decrement (--)
+console.log(--x);
+
+console.log(x + y);
